1e6485f11783f7c0a9be135b32b954cff4b9e8f5
nathanw23 <94845836+nathanw23@users.noreply.github.com>
2026-02-18 15:34:18 +0000

    Updated test_sweetspot.py

--- tests/test_sweetspot.py
+++ tests/test_sweetspot.py
@@ -1,4 +1,6 @@
 from sweetspot import LargerIsBetter
 import numpy as np
 
-def test_basic(): f=LargerIsBetter(0,10); assert f(5)>0
+def test_basic():
+    f = LargerIsBetter(0, 10)
+    assert f(5) > 0
